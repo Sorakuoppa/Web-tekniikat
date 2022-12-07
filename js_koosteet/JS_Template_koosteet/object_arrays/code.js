e4e4d7b3d36c09eec0d3a3025d6d031cd578d209
Sorakuoppa <n2hyti00@students.oamk.fi>
2022-12-07 13:24:57 +0200

    tehtävä 1 tehty

--- js_koosteet/JS_Template_koosteet/object_arrays/code.js
+++ js_koosteet/JS_Template_koosteet/object_arrays/code.js
@@ -1,15 +1,28 @@
 //Luo tähän taulukko käyttäjiä varten.
 
-document.getElementById("submit_button").addEventListener(submitUser);
-
-
+document.getElementById("submit_button").addEventListener("click", submitUser);
+let users = [];
+let form = document.querySelector("form");
 /**
  * @param {Event} e 
  */
 function submitUser(e){
     e.preventDefault(); //estetään sivun päivitys, kun nappia painetaan
-
+    let formData = new FormData(form);
     //Tee tähän koodi, joka tallettaa uuden käyttäjän tiedot oliona taulukkoon
+    let un = formData.get("uname");
+    let pw = formData.get("pw");
+    let user = {
+        username: un,
+        password: pw
+    }
+/*  voi myös lyhentää tälleen, koska submuuttuja on sama kuin jäsenmuuttujan nimi:   
+    let user2 = {
+        un,
+        pw
+    } */
+
+    users.push(user);
 
     //Tulostetaan aina lopuksi kaikki lisätyt käyttäjänimet
     printUsers();
@@ -17,4 +30,8 @@ function submitUser(e){
 
 function printUsers(){
     //Tulosta tähän kaikkien käyttäjien nimi taulukosta.
+    console.log("-----");
+    for (const user of users) {
+        console.log(user.username)
+    }
 }
